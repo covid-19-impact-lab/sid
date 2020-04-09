7b5f2eddc6c4dd29c113faedd2a02b0882dc3197
Janos Gabler <janos.gabler@gmail.com>
2020-04-09 15:31:19 +0200

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
-p = find_packages()
-print(p)
 
 setup(
-    name="utilities",
+    name="simulator",
     version="0.0.1",
     description=("Simulate the spread of covid-19 with different policies."),
     license="MIT",
     url="https://github.com/covid-19-impact-lab/simulator",
     author="Janos Gabler and Tobias Raabe",
     author_email="janos.gabler@gmail.com",
-    packages=p,
+    packages=find_packages(),
     zip_safe=False,
 )
