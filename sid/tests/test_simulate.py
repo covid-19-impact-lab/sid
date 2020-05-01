38e877bac1f202cdd02451c3eb6e61ee664c5a83
Klara Roehrl <klara.roehrl@gmail.com>
2020-05-01 12:56:16 +0200

    Add tests without markers back in.

--- sid/tests/test_simulate.py
+++ sid/tests/test_simulate.py
@@ -1,5 +1,10 @@
+import os
+from pathlib import Path
+
+import nbformat
 import numpy as np
 import pandas as pd
+from nbconvert.preprocessors import ExecutePreprocessor
 
 from sid.parse_model import parse_duration
 from sid.simulate import _process_initial_states
@@ -45,3 +50,18 @@ def test_process_data_w_multiindex():
     simulation_results = _process_simulation_results(to_concat, index_names)
 
     assert simulation_results.index.names == ["date", "index_a", "index_b"]
+
+
+# @pytest.mark.optional
+def test_simulate_notebook():
+    """Run the simulation notebook.
+
+   source: https://nbconvert.readthedocs.io/en/latest/execute_api.html
+
+   """
+    repo_path = Path(__file__).resolve().parent.parent.parent
+    tutorials_path = repo_path / "docs" / "source" / "tutorials"
+    with open(tutorials_path / "simulation.ipynb") as f:
+        notebook = nbformat.read(f, as_version=4)
+    os.chdir(tutorials_path)
+    ExecutePreprocessor(timeout=600).preprocess(notebook)
--- sid/tests/test_visualize_simulation_results.py
+++ sid/tests/test_visualize_simulation_results.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import pandas as pd
 import pytest
 
 import sid.visualize_simulation_results as vsr
@@ -27,3 +30,40 @@ def test_create_folders(tmp_path):
     vsr._create_folders(tmp_path, bg_vars)
     for path in expected:
         assert path.exists()
+
+
+# @pytest.mark.optional
+def test_load_data_path(keep_vars):
+    path = Path(__file__).resolve().parent / "simulation_results" / "001.pkl"
+    expected_name = "001"
+    expected_df = pd.read_pickle(path)[keep_vars]
+    name, df = vsr._load_data(path, keep_vars=keep_vars, i=100)
+    assert expected_name == name
+    pd.testing.assert_frame_equal(expected_df, df)
+
+
+# @pytest.mark.optional
+def test_load_data_df(keep_vars):
+    path = Path(__file__).resolve().parent / "simulation_results" / "001.pkl"
+    input_df = pd.read_pickle(path)
+    expected_df = pd.read_pickle(path)[keep_vars]
+    name, df = vsr._load_data(input_df, keep_vars=keep_vars, i=100)
+    assert name == 100
+    pd.testing.assert_frame_equal(expected_df, df)
+
+
+# @pytest.mark.optional
+@pytest.mark.filterwarnings("ignore::RuntimeWarning")
+def test_visualize_simulation_results(tmp_path):
+    path = Path(__file__).resolve().parent / "simulation_results"
+    data = [path / "001.pkl", path / "002.pkl"]
+    bg_vars = ["age_group"]
+    infection_vars = ["immune", "infectious"]
+    # just check that this does not raise any Errors:
+    vsr.visualize_simulation_results(
+        data=data,
+        outdir_path=tmp_path,
+        infection_vars=infection_vars,
+        background_vars=bg_vars,
+        window_length=2,
+    )
