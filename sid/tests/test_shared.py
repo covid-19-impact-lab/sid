de5f531710d94fd86d127bfffbcf6f1281fa9474
Tobias Raabe <raabe@posteo.de>
2020-05-03 14:22:18 +0200

    Visualize uncertainty. (#11)
    Co-authored-by: Klara Roehrl <klara.roehrl@gmail.com>

--- sid/tests/test_shared.py
+++ sid/tests/test_shared.py
@@ -1,5 +1,9 @@
+import pandas as pd
 import pytest
+from pytest import approx
 
+from sid.shared import calculate_r_effective
+from sid.shared import calculate_r_zero
 from sid.shared import factorize_assortative_variables
 
 
@@ -24,3 +28,38 @@ def test_factorize_assortative_variables(initial_states, assort_by, expected):
     _, group_code_values = factorize_assortative_variables(initial_states, assort_by)
 
     assert set(group_code_values) == set(expected)
+
+
+@pytest.fixture
+def data_for_replication_numbers():
+    df = pd.DataFrame()
+    df["cd_infectious_false"] = [-2, -1, -1, 0] + [2, 5]
+    df["infection_counter"] = [2, 1, 1, 1] + [0, 0]
+    df["immune"] = [True] * 4 + [False, False]
+    return df
+
+
+def test_r_effective_large(data_for_replication_numbers):
+    expected = 5 / 4
+    res = calculate_r_effective(data_for_replication_numbers, 2)
+    assert res == expected
+
+
+def test_r_effective_small(data_for_replication_numbers):
+    expected = 3 / 3
+    res = calculate_r_effective(data_for_replication_numbers, 1)
+    assert res == expected
+
+
+def test_r_zero_few_susceptible(data_for_replication_numbers):
+    res = calculate_r_zero(data_for_replication_numbers, 2)
+    expected = (5 / 4) / (2 / 6)
+    assert res == approx(expected)
+
+
+def test_r_zero_all_susceptible(data_for_replication_numbers):
+    df = data_for_replication_numbers
+    df["immune"] = False
+    expected = (3 / 3) / (6 / 6)
+    res = calculate_r_zero(data_for_replication_numbers, 1)
+    assert res == expected
