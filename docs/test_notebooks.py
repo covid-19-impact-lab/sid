9fec623aa23c23685938e2a7a9a1534a3ff630b1
Klara Roehrl <klara.roehrl@gmail.com>
2020-05-03 15:01:28 +0200

    check if test_notebooks is run.

--- docs/test_notebooks.py
+++ docs/test_notebooks.py
@@ -11,14 +11,8 @@ def test_notebooks():
     source: https://nbconvert.readthedocs.io/en/latest/execute_api.html
 
     """
-    import nbformat
-    from nbconvert.preprocessors import ExecutePreprocessor
 
     repo_path = Path(__file__).resolve().parent.parent
     tutorials_path = repo_path / "docs" / "source" / "tutorials"
-    ipynbs = list(tutorials_path.glob("*.ipynb"))
-    for nb_name in ipynbs:
-        with open(tutorials_path / nb_name) as f:
-            notebook = nbformat.read(f, as_version=4)
     os.chdir(tutorials_path)
-    ExecutePreprocessor(timeout=600).preprocess(notebook)
+    assert True is False, "test_notebooks ran!"
