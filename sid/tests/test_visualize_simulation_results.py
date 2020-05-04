20401f736913259d7915a1a505665d05ff043ecf
Klara Roehrl <klara.roehrl@gmail.com>
2020-05-04 13:54:13 +0200

    Make imports for tests explicit.

--- sid/tests/test_visualize_simulation_results.py
+++ sid/tests/test_visualize_simulation_results.py
@@ -3,7 +3,10 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
-import sid.visualize_simulation_results as vsr
+from sid.visualize_simulation_results import _create_folders
+from sid.visualize_simulation_results import _load_data
+from sid.visualize_simulation_results import _nice_str
+from sid.visualize_simulation_results import visualize_simulation_results
 
 
 @pytest.fixture
@@ -13,21 +16,21 @@ def keep_vars():
 
 def test_nice_str():
     s = "hello_world"
-    res = vsr._nice_str(s)
+    res = _nice_str(s)
     expected = "Hello World"
     assert res == expected
 
 
 def test_nice_str_no_change():
     s = "Bye World"
-    res = vsr._nice_str(s)
+    res = _nice_str(s)
     assert res == s
 
 
 def test_create_folders(tmp_path):
     bg_vars = ["age_group", "gender", "sector", "region"]
     expected = [tmp_path / "general"] + [tmp_path / x for x in bg_vars]
-    vsr._create_folders(tmp_path, bg_vars)
+    _create_folders(tmp_path, bg_vars)
     for path in expected:
         assert path.exists()
 
@@ -37,7 +40,7 @@ def test_load_data_path(keep_vars):
     path = Path(__file__).resolve().parent / "simulation_results" / "01.pkl"
     expected_name = "01"
     expected_df = pd.read_pickle(path)[keep_vars]
-    name, df = vsr._load_data(path, keep_vars=keep_vars, i=100)
+    name, df = _load_data(path, keep_vars=keep_vars, i=100)
     assert expected_name == name
     pd.testing.assert_frame_equal(expected_df, df)
 
@@ -47,7 +50,7 @@ def test_load_data_df(keep_vars):
     path = Path(__file__).resolve().parent / "simulation_results" / "01.pkl"
     input_df = pd.read_pickle(path)
     expected_df = pd.read_pickle(path)[keep_vars]
-    name, df = vsr._load_data(input_df, keep_vars=keep_vars, i=100)
+    name, df = _load_data(input_df, keep_vars=keep_vars, i=100)
     assert name == 100
     pd.testing.assert_frame_equal(expected_df, df)
 
@@ -60,7 +63,7 @@ def test_visualize_simulation_results(tmp_path):
     bg_vars = ["age_group"]
     infection_vars = ["immune", "infectious"]
     # just check that this does not raise any Errors:
-    vsr.visualize_simulation_results(
+    visualize_simulation_results(
         data=data,
         outdir_path=tmp_path,
         infection_vars=infection_vars,
