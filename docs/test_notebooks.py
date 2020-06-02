32e49950837f6bd335cb4338ea1775b0516a35bd
Klara Roehrl <klara.roehrl@gmail.com>
2020-06-02 22:04:38 +0200

    Make notebook run as separate tests.

--- docs/test_notebooks.py
+++ docs/test_notebooks.py
@@ -4,26 +4,31 @@ from pathlib import Path
 import pytest
 
 
+def find_notebooks():
+    repo_path = Path(__file__).resolve().parent.parent
+    src_path = repo_path / "docs" / "source"
+
+    ipynbs = list(src_path.glob("*/*.ipynb"))
+    # Remove checkpoints.
+    ipynbs = [ipynb for ipynb in ipynbs if ".ipynb_checkpoints" not in ipynb.as_posix()]
+    return ipynbs
+
+
+NOTEBOOKS = find_notebooks()
+
+
 @pytest.mark.optional
-def test_notebooks():
+@pytest.mark.parametrize("nb_path", NOTEBOOKS)
+def test_notebooks(nb_path):
     """Run the simulation notebook.
 
     source: https://nbconvert.readthedocs.io/en/latest/execute_api.html
 
     """
-
-    repo_path = Path(__file__).resolve().parent.parent
-    tutorials_path = repo_path / "docs" / "source" / "tutorials"
-    os.chdir(tutorials_path)
-
-    ipynbs = list(tutorials_path.glob("*.ipynb"))
-    # Remove checkpoints.
-    ipynbs = [ipynb for ipynb in ipynbs if ".ipynb_checkpoints" not in ipynb.as_posix()]
-
     import nbformat
     from nbconvert.preprocessors import ExecutePreprocessor
 
-    for nb_name in ipynbs:
-        with open(nb_name) as f:
-            notebook = nbformat.read(f, as_version=4)
-        ExecutePreprocessor(timeout=600).preprocess(notebook)
+    os.chdir(nb_path.parent)
+    with open(nb_path) as f:
+        notebook = nbformat.read(f, as_version=4)
+    ExecutePreprocessor(timeout=1200).preprocess(notebook)
