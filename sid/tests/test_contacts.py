693214e370243972b386299cab0d896c2237ae98
Klara Roehrl <klara.roehrl@gmail.com>
2020-06-02 15:47:14 +0200

    Update params to not have empty index level values.

--- sid/tests/test_contacts.py
+++ sid/tests/test_contacts.py
@@ -160,7 +160,9 @@ def test_calculate_infections():
     params = pd.DataFrame(
         columns=["value"],
         data=1.0,
-        index=pd.MultiIndex.from_tuples([("infection_prob", "households", None)]),
+        index=pd.MultiIndex.from_tuples(
+            [("infection_prob", "households", "households")]
+        ),
     )
 
     indexers = {"households": create_group_indexer(states, ["households"])}
