cb2546fbf43638c75e1a0dd233cabf2d832f7387
Tobias Raabe <raabe@posteo.de>
2020-11-27 08:41:41 +0100

    Reimplement 'Save which contact model caused an infecetion (#42)'.

--- src/sid/simulate.py
+++ src/sid/simulate.py
@@ -231,24 +231,32 @@ def _simulate(
         seed=seed,
         optional_state_columns=optional_state_columns,
     )
+    code_to_contact_model = dict(enumerate(contact_models))
+
     for date in duration["dates"]:
         states["date"] = date
 
         contacts = calculate_contacts(
-            contact_models, contact_policies, states, params, date
+            contact_models=contact_models,
+            contact_policies=contact_policies,
+            states=states,
+            params=params,
+            date=date,
         )
 
         (
             newly_infected_contacts,
             n_has_additionally_infected,
             newly_missed_contacts,
+            was_infected_by_contact,
         ) = calculate_infections_by_contacts(
-            states,
-            contacts,
-            params,
-            indexers,
-            cum_probs,
-            seed,
+            states=states,
+            contacts=contacts,
+            params=params,
+            indexers=indexers,
+            group_cdfs=cum_probs,
+            code_to_contact_model=code_to_contact_model,
+            seed=seed,
         )
         newly_infected_events = calculate_infections_by_events(states, params, events)
 
@@ -281,6 +289,7 @@ def _simulate(
             indexers=indexers,
             contacts=contacts,
             to_be_processed_test=to_be_processed_tests,
+            was_infected_by_contact=was_infected_by_contact,
         )
 
         _dump_periodic_states(states, columns_to_keep, path, date)
