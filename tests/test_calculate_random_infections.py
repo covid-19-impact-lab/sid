910ff82b51202126feeb369aceba4eeaa7ad3d2c
Tobias Raabe <raabe@posteo.de>
2021-03-07 14:01:13 +0100

    Add two tests for numba functions with multiple virus strains.

--- tests/test_calculate_random_infections.py
+++ tests/test_calculate_random_infections.py
@@ -1,6 +1,5 @@
 import numba as nb
 import numpy as np
-import pytest
 from sid.contacts import _calculate_infections_by_random_contacts
 
 
@@ -105,6 +104,50 @@ def test_random_contact_immune_and_people_without_contacts_are_not_infected():
     assert (was_infected == [-1, -1, -1, -1, -1]).all()
 
 
-@pytest.mark.xfail
 def test_multiple_virus_strains_spread_in_different_random_groups():
-    pass
+    random_contacts = np.array([1, 1, 1, 1]).reshape(-1, 1)
+    infectious = np.array([True, False, True, False])
+    immune = np.array([True, False, True, False])
+
+    group_codes = np.array([0, 0, 1, 1]).reshape(-1, 1)
+
+    sub_indexers = nb.typed.List()
+    sub_indexers.append(np.array([0, 1]))
+    sub_indexers.append(np.array([2, 3]))
+    indexers = nb.typed.List()
+    indexers.append(sub_indexers)
+
+    infection_counter = np.zeros(4, dtype=np.int_)
+    infection_probability_multiplier = np.ones(len(random_contacts))
+
+    assortative_matching_cum_probs = nb.typed.List()
+    assortative_matching_cum_probs.append(np.array([1, 0, 0, 1]).reshape(2, 2))
+
+    virus_strain = np.array([0, -1, 1, -1])
+    virus_strains_multipliers = np.array([1, 1])
+
+    (
+        newly_infected,
+        infection_counter,
+        immune,
+        missed,
+        was_infected,
+    ) = _calculate_infections_by_random_contacts(
+        random_contacts,
+        infectious,
+        immune,
+        virus_strain,
+        group_codes,
+        assortative_matching_cum_probs,
+        indexers,
+        infection_probability_multiplier,
+        virus_strains_multipliers,
+        infection_counter,
+        0,
+    )
+
+    assert (newly_infected == [-1, 0, -1, 1]).all()
+    assert (infection_counter == [1, 0, 1, 0]).all()
+    assert (immune == [True, True, True, True]).all()
+    assert (missed == 0).all()
+    assert (was_infected == [-1, 0, -1, 0]).all()
--- tests/test_calculate_recurrent_infections.py
+++ tests/test_calculate_recurrent_infections.py
@@ -1,6 +1,5 @@
 import numba as nb
 import numpy as np
-import pytest
 from sid.contacts import _calculate_infections_by_recurrent_contacts
 
 
@@ -139,6 +138,46 @@ def test_infections_can_be_scaled_with_multiplier():
     assert np.isclose(immune.sum(), n_individuals / 2, atol=1e2)
 
 
-@pytest.mark.xfail
 def test_multiple_virus_strains_spread_in_different_recurrent_groups():
-    pass
+    recurrent_contacts = np.array([True, True, True, True]).reshape(-1, 1)
+    infectious = np.array([True, False, True, False])
+    immune = np.array([True, False, True, False])
+
+    group_codes = np.array([0, 0, 1, 1]).reshape(-1, 1)
+
+    sub_indexers = nb.typed.List()
+    sub_indexers.append(np.array([0, 1]))
+    sub_indexers.append(np.array([2, 3]))
+    indexers = nb.typed.List()
+    indexers.append(sub_indexers)
+
+    infection_probabilities = np.array([1])
+    infection_counter = np.zeros(4, dtype=np.int_)
+    infection_probability_multiplier = np.ones(len(recurrent_contacts))
+
+    virus_strain = np.array([0, -1, 1, -1])
+    virus_strains_multipliers = np.array([1, 1])
+
+    (
+        newly_infected,
+        infection_counter,
+        immune,
+        was_infected,
+    ) = _calculate_infections_by_recurrent_contacts(
+        recurrent_contacts,
+        infectious,
+        immune,
+        virus_strain,
+        group_codes,
+        indexers,
+        infection_probabilities,
+        infection_probability_multiplier,
+        virus_strains_multipliers,
+        infection_counter,
+        0,
+    )
+
+    assert (newly_infected == [-1, 0, -1, 1]).all()
+    assert (infection_counter == [1, 0, 1, 0]).all()
+    assert (immune == [True, True, True, True]).all()
+    assert (was_infected == [-1, 0, -1, 0]).all()
