3100a9813f9b3b334b769129e6d47184325235d2
Tobias Raabe <raabe@posteo.de>
2021-03-25 20:57:46 +0100

    Finish rapid tests.

--- src/sid/rapid_tests.py
+++ src/sid/rapid_tests.py
@@ -2,7 +2,9 @@ import itertools
 from typing import Callable
 from typing import Optional
 
+import numpy as np
 import pandas as pd
+from sid.shared import boolean_choices
 from sid.validation import validate_return_is_series_or_ndarray
 
 
@@ -13,6 +15,7 @@ def perform_rapid_tests(
     rapid_tests_models: Optional[Callable],
     seed: itertools.count,
 ) -> pd.DataFrame:
+    """Perform testing with rapid tests."""
     if rapid_tests_models:
         receives_rapid_test = _compute_who_receives_rapid_tests(
             date, states, params, rapid_tests_models, seed
@@ -30,6 +33,13 @@ def perform_rapid_tests(
 
 
 def _compute_who_receives_rapid_tests(date, states, params, rapid_tests_models, seed):
+    """Compute who receives rapid tests.
+
+    We loop over all rapid tests models and collect newly allocated rapid tests in
+    ``receives_rapid_test``. A copy of the series is passed to each rapid test model so
+    that the user is aware of who is tested, but cannot alter the existing assignment.
+
+    """
     receives_rapid_test = pd.Series(index=states.index, data=False)
 
     for model in rapid_tests_models.values():
@@ -53,13 +63,41 @@ def _compute_who_receives_rapid_tests(date, states, params, rapid_tests_models, 
     return receives_rapid_test
 
 
-def _sample_test_outcome(states, receives_rapid_test, params, seed):  # noqa: U100
-    return states
+def _sample_test_outcome(states, receives_rapid_test, params, seed):
+    """Sample the outcomes of the rapid tests.
+
+    For those who are infectious, sensitivity gives us the probability that they are
+    also tested positive.
+
+    For those who are not infectious, 1 - specificity gives us the probability that they
+    are falsely tested positive.
+
+    """
+    np.random.seed(next(seed))
+
+    is_tested_positive = pd.Series(index=states.index, data=False)
+
+    sensitivity = params.loc[("rapid_test", "sensitivity", "sensitivity"), "value"]
+    receives_test_and_is_infectious = states["infectious"] & receives_rapid_test
+    is_truly_positive = boolean_choices(
+        np.full(receives_test_and_is_infectious.sum(), sensitivity)
+    )
+
+    specificity = params.loc[("rapid_test", "specificity", "specificity"), "value"]
+    receives_test_and_is_not_infectious = ~states["infectious"] & receives_rapid_test
+    is_falsely_positive = boolean_choices(
+        np.full(receives_test_and_is_not_infectious.sum(), 1 - specificity)
+    )
+
+    is_tested_positive.loc[is_truly_positive | is_falsely_positive] = True
+
+    return is_tested_positive
 
 
 def _update_states_with_rapid_tests_outcomes(
     states, receives_rapid_test, is_tested_positive
 ):
+    """Updates states with outcomes of rapid tests."""
     states[receives_rapid_test, "cd_received_rapid_test"] = 0
     states[is_tested_positive, "is_tested_positive_by_rapid_test"] = True
 
