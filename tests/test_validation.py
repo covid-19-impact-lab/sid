88e809674842de481fe4e94c6d63f8d0a6093089
Tobias Raabe <raabe@posteo.de>
2021-04-07 07:56:42 +0200

    some test cases.

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -2,6 +2,139 @@ from contextlib import ExitStack as does_not_raise  # noqa: N813
 
 import pytest
 from sid.validation import _validate_model_function
+from sid.validation import validate_contact_policies
+from sid.validation import validate_testing_models
+from sid.validation import validate_vaccination_models
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "contact_policies, contact_models, expectation",
+    [
+        pytest.param(
+            None,
+            {},
+            pytest.raises(ValueError, match="'contact_policies' must be "),
+            id="test is dictionary.",
+        ),
+        pytest.param(
+            {"model": None},
+            {},
+            pytest.raises(ValueError, match="Contact policy"),
+            id="test model is dictionary.",
+        ),
+        pytest.param(
+            {"model": {}},
+            {},
+            pytest.raises(ValueError, match="The contact policy"),
+            id="test missing keys.",
+        ),
+        pytest.param(
+            {
+                "model": {
+                    "policy": None,
+                    "start": None,
+                    "end": None,
+                    "affected_contact_model": None,
+                }
+            },
+            {},
+            pytest.raises(ValueError, match="The contact policy 'model' affects"),
+            id="test affected model is present",
+        ),
+        pytest.param(
+            {
+                "model": {
+                    "policy": lambda x: None,
+                    "start": None,
+                    "end": None,
+                    "affected_contact_model": "contact_model",
+                }
+            },
+            {"contact_model": None},
+            pytest.raises(ValueError, match="The model 'model' of 'contact_policies'"),
+            id="test function",
+        ),
+        pytest.param(
+            {
+                "model": {
+                    "policy": 0.5,
+                    "start": None,
+                    "end": None,
+                    "affected_contact_model": "model",
+                }
+            },
+            {"model": {"is_recurrent": True}},
+            pytest.raises(ValueError, match="Specifying multipliers"),
+            id="test number for recurrent models.",
+        ),
+        pytest.param(
+            {
+                "model": {
+                    "policy": -1,
+                    "start": None,
+                    "end": None,
+                    "affected_contact_model": "model",
+                }
+            },
+            {"model": {"is_recurrent": False}},
+            pytest.raises(ValueError, match="The policy of contact policy 'model'"),
+            id="test number for random models.",
+        ),
+        pytest.param(
+            {
+                "model": {
+                    "policy": None,
+                    "start": None,
+                    "end": None,
+                    "affected_contact_model": "model",
+                }
+            },
+            {"model": {"is_recurrent": False}},
+            pytest.raises(ValueError, match="The 'policy' entry of contact policy"),
+            id="test invalid policy.",
+        ),
+    ],
+)
+def test_validate_contact_policies(contact_policies, contact_models, expectation):
+    with expectation:
+        validate_contact_policies(contact_policies, contact_models)
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "demand, allocation, processing, expectation",
+    [
+        (
+            None,
+            {},
+            {},
+            pytest.raises(ValueError, match="'testing_demand_models' must be"),
+        ),
+        ({"model": None}, {}, {}, pytest.raises(ValueError, match="Each model")),
+        ({"model": {}}, {}, {}, pytest.raises(TypeError, match="The model")),
+    ],
+)
+def test_validate_testing_models(demand, allocation, processing, expectation):
+    with expectation:
+        validate_testing_models(demand, allocation, processing)
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "models, expectation",
+    [
+        (None, pytest.raises(ValueError, match="'vaccination_models' must be")),
+        ({"model": None}, pytest.raises(ValueError, match="Vaccination model")),
+        (
+            {"model": {"model": lambda receives_vaccine, states, params, seed: None}},
+            does_not_raise(),
+        ),
+    ],
+)
+def test_validate_vaccination_models(models, expectation):
+    with expectation:
+        validate_vaccination_models(models)
 
 
 @pytest.mark.unit
