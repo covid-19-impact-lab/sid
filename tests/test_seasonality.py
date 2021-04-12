e4f53eddff3fb995875ae1c0321defbdf30c1787
Tobias Raabe <raabe@posteo.de>
2021-04-13 00:04:08 +0200

    add end-to-end test.

--- tests/test_seasonality.py
+++ tests/test_seasonality.py
@@ -4,7 +4,36 @@ from contextlib import ExitStack as does_not_raise  # noqa: N813
 import numpy as np
 import pandas as pd
 import pytest
+from resources import CONTACT_MODELS
+from sid import get_simulate_func
 from sid.seasonality import prepare_seasonality_factor
+
+
+@pytest.mark.end_to_end
+def test_simulate_a_simple_model(params, initial_states, tmp_path):
+    simulate = get_simulate_func(
+        params=params,
+        initial_states=initial_states,
+        contact_models=CONTACT_MODELS,
+        seasonality_factor_model=lambda params, dates, seed: pd.Series(
+            index=dates, data=1
+        ),
+        saved_columns={"other": ["channel_infected_by_contact"]},
+        path=tmp_path,
+        seed=144,
+    )
+
+    result = simulate(params)
+
+    time_series = result["time_series"].compute()
+    last_states = result["last_states"].compute()
+
+    for df in [time_series, last_states]:
+        assert isinstance(df, pd.DataFrame)
+        assert set(df["channel_infected_by_contact"].cat.categories) == {
+            "not_infected_by_contact",
+            "standard",
+        }
 
 
 @pytest.mark.unit
