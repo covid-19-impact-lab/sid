8ac3b8b9e8fdfb4017e419c4c95d7161c5cda5d7
Tobias Raabe <raabe@posteo.de>
2021-04-13 09:15:15 +0200

    Implement policies working on all contacts.

--- src/sid/policies.py
+++ src/sid/policies.py
@@ -16,26 +16,29 @@ def apply_contact_policies(
 ) -> pd.DataFrame:
     """Apply policies to contacts."""
     for name, policy in contact_policies.items():
+        if policy["start"] <= date <= policy["end"]:
+            func = policy["policy"]
 
-        affected_cm = policy["affected_contact_model"]
-        if affected_cm in contacts.columns:
-            if policy["start"] <= date <= policy["end"]:
-                func = policy["policy"]
-                model_specific_contacts = contacts[affected_cm]
+            affected_cm = policy.get("affected_contact_model")
+            affected_contacts = (
+                contacts if affected_cm is None else contacts[affected_cm]
+            )
 
-                if isinstance(policy["policy"], (float, int)):
-                    model_specific_contacts = model_specific_contacts * policy["policy"]
-                else:
-                    model_specific_contacts = func(
-                        states=states,
-                        contacts=model_specific_contacts,
-                        seed=next(seed),
-                    )
-
-                model_specific_contacts = validate_return_is_series_or_ndarray(
-                    model_specific_contacts, name, "contact_policies", states.index
+            if isinstance(policy["policy"], (float, int)):
+                affected_contacts = affected_contacts * policy["policy"]
+            else:
+                affected_contacts = func(
+                    states=states,
+                    contacts=affected_contacts,
+                    seed=next(seed),
                 )
 
-                contacts[affected_cm] = model_specific_contacts
+            if affected_cm is None:
+                contacts = affected_contacts
+            else:
+                affected_contacts = validate_return_is_series_or_ndarray(
+                    affected_contacts, name, "contact_policies", states.index
+                )
+                contacts[affected_cm] = affected_contacts
 
     return contacts
